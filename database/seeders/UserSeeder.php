2f861dfa97b1da2ec3a48c7dc04d342541e74d6a
Abrera Kenji Ken L. <kenjiabrera2@gmail.com>
2026-04-22 21:16:03 +0800

    env. and UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the users table.
+     */
+    public function run(): void
+    {
+        // Create 500 random users
+        User::factory(500)->create();
+
+        // Create a test user
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
